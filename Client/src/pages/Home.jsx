ee2f93e48b26bb34e45f458b506793332a2283df
PavanKumar-sudo <pavanvinjamuri212@gmail.com>
2025-09-18 17:10:23 -0600

    first-commit

---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AItools from '../components/AItools'
+import Testimonial from '../components/Testimonial'
+import Plan from '../components/Plan'
+import Footer from '../components/footer'
+
+
+const Home = () => {
+  return (
+   <>
+    <Navbar />
+    <Hero />
+    <AItools />
+    <Testimonial />
+    <Plan />
+    <Footer />
+   </>
+  )
+}
+
+export default Home
---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+import { clerkClient } from "@clerk/express";
+
+export const auth = async (req, res, next) => {
+  try {
+    const { userId, has } = req.auth();  // ✅ req.auth is an object, not a function
+    const hasPremiumPlan = await has({plan: 'premium'});
+    const user=await clerkClient.users.getUser(userId);
+    if(!hasPremiumPlan && user.privateMetadata.free_usage){
+            req.free_usage = user.privateMetadata.free_usage
+        } else {
+            await clerkClient.users.updateUserMetadata(userId,{
+                privateMetadata: {
+                    free_usage: 0
+                }
+            })
+            req.free_usage = 0;
+        }
+        req.plan = hasPremiumPlan ? 'premium' : 'free';
+        next()
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+};
